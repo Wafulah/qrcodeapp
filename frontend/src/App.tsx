029d52db4384044888288ee1b590c9eda6f52e9b
Wafulah <wafulahvictor@gmail.com>
2023-05-05 11:45:57 -0700

    frontend

---
+++ frontend/src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import "../dist/output.css";
+
+function App() {
+  return (
+    <div className="bg-red-100">
+        <div>
+        <p className="text-lg text-blue-500">App</p>
+        </div>
+        <div className='bg-red-500'>
+            hello
+        </div>
+        </div>
+  )
+}
+
+export default App;
